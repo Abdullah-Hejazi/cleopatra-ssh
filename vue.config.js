0cf2c9a03dbd3ca3a4c3608f36b33cdb6ce55124
Abdullah Hejazi <a.m.a.hejazi@gmail.com>
2022-12-26 11:22:15 +0200

    fixed linux icons in .deb

--- vue.config.js
+++ vue.config.js
@@ -4,29 +4,21 @@ module.exports = {
             nodeIntegration: true,
             externals:['ssh2-promise'],
             builderOptions: {
-                icon: 'build/icon.png',
                 win: {
                     target: 'portable',
                     asar: true,
-                    icon: 'build/icon.ico'
+                    icon: 'icon.ico'
                 },
                 linux:{
                     target:[
                         "deb",
                     ],
                     category: "Development",
-                    icon: 'build/256x256.png',
 
                     vendor: 'Abdullah Hejazi',
                     maintainer: 'Abdullah Hejazi',
                     synopsis: 'A GUI SSH Client that looks like a desktop',
                     description: 'An ssh client that converts your ssh connection to a full blown desktop application'
-                },
-                deb: {
-                    icon: 'build/256x256.png',
-                },
-                directories:{
-                    buildResources: "build"
                 }
               }
         }
